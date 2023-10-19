3bea69909c499a455e11bd05925cfbe2f2fcfbf8
Martin Simpson <martin.simpson@ubiquitypress.com>
2023-10-19 16:47:00 +0700

    Added URL lock

--- lib/hyku_plugin_google_cloud_url_mask/engine.rb
+++ lib/hyku_plugin_google_cloud_url_mask/engine.rb
@@ -1,3 +1,7 @@
+require_relative "../app/controllers/hyku_plugin_cloud_url_mask/application_controller"
+require_relative "../app/controllers/hyku_plugin_cloud_url_mask/app/files_controller_behavior"
+require_relative "../app/services/file_signed_url_service"
+
 module HykuPluginGoogleCloudUrlMask
   class Engine < ::Rails::Engine
     isolate_namespace HykuPluginGoogleCloudUrlMask
