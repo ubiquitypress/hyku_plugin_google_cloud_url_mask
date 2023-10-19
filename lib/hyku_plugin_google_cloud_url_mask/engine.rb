2b65b492a1b596a57c230ab188dac38a28969202
Martin Simpson <martin.simpson@ubiquitypress.com>
2023-10-19 20:28:39 +0700

    Force to_prepare

--- lib/hyku_plugin_google_cloud_url_mask/engine.rb
+++ lib/hyku_plugin_google_cloud_url_mask/engine.rb
@@ -9,7 +9,6 @@ module HykuPluginGoogleCloudUrlMask
       ::Hyku::API::V1::FilesController.include HykuPluginGoogleCloudUrlMask::FilesControllerBehavior
     end
     
-    method = Rails.env.development? ? :to_prepare : :after_initialize
-    config.send(method) { HykuPluginGoogleCloudUrlMask::Engine.mixins }
+    config.send(:to_prepare) { HykuPluginGoogleCloudUrlMask::Engine.mixins }
   end
 end
