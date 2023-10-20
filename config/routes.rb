130de5988afc23c64982cd01800ea44913442690
Martin Simpson <martin.simpson@ubiquitypress.com>
2023-10-20 21:10:20 +0700

    Changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+HykuPluginGoogleCloudUrlMask::Engine.routes.draw do
   get "/api/v1/tenant/:tenant_id/files/:id/work", to: "/hyku/api/v1/files#work"
   get "/api/v1/tenant/:tenant_id/files/:id/download", to: "/hyku/api/v1/files#download"
 end
